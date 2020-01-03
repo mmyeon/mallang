e82c73f1523450cdb639e37e42c6f1a9de59cf27
Meagle <mindfulyeon@gmail.com>
2020-01-03 16:17:33 +0900

    랜딩페이지  >  달력 계산 부분 삭제

--- public/js/main.js
+++ public/js/main.js
@@ -2,14 +2,3 @@
 function myFunction() {
   location.assign("second.html");
 }
-
-//말랑이 만난 날짜 계산
-var date1 = new Date("10/08/2018");
-var date2 = new Date();
-
-var Difference_In_Time = date2.getTime() - date1.getTime();
-
-var Difference_In_Days =
-  Math.floor(Difference_In_Time / (1000 * 3600 * 24)) + 1;
-
-document.getElementById("date").innerHTML = Difference_In_Days;
